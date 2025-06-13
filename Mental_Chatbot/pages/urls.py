1a9ccd137fb2661ac7bd0e395a93a212481bff4f
Ghalib92 <ghalibmohammad92@gmail.com>
2025-06-13 14:37:46 +0300

    Initial commit, folder structure, chat bot integration,landing page

---
+++ Mental_Chatbot/pages/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name = 'home'),
+    path("chatbot-page/", views.chatbot_page, name="chatbot-page"),
+    path('get/',views.get_response, name='get_response'),
+   
+     
+     
+     ]
---
+++ Mental_Chatbot/pages/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAI
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+ 
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+
+embeddings = download_hugging_face_embeddings()
+
+
+index_name = "mentalbot"
+
+# Embed each chunk and upsert the embeddings into your Pinecone index.
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k":3})
+
+
+llm = OpenAI(temperature=0.4, max_tokens=500)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+ 
+
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse,HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+
+
+def home (request):
+    return render (request, 'index.html')
+
+
+def chatbot_page(request):
+    return render(request, 'chat.html')
+
+def get_response(request):
+    if request.method == 'POST':
+        msg = request.POST.get('msg')
+        if msg:
+            response = rag_chain.invoke({"input": msg})
+            return HttpResponse(response["answer"])
+    else:
+        return HttpResponse("Invalid request")
